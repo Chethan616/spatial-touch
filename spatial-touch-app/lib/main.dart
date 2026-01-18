333d3939cb5546879f2e1bdddc2e42d0252d3a37
Chethan Krishna <106482422+Chethan616@users.noreply.github.com>
2026-01-18 19:25:02 +0530

    Add initial comment to main.dart

--- spatial-touch-app/lib/main.dart
+++ spatial-touch-app/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'src/app.dart';
-
+//initial
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   GoogleFonts.config.allowRuntimeFetching = true;
